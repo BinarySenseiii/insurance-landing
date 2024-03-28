a50a3e7684804922f5b0524850a26bff24add463
Faisal Tariq <faisaltariq183@gmail.com>
2024-03-28 23:34:25 +0400

    init-shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,9 @@
+import {Button} from '@/components/ui/button'
+
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>
+  return (
+    <h1 className="text-3xl font-bold underline">
+      <Button variant="outline">Button</Button>
+    </h1>
+  )
 }
