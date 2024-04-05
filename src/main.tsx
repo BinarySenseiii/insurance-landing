b27b981551c80f35d073e0bf88a3d449f52e7cef
Faisal Tariq <faisaltariq183@gmail.com>
2024-04-05 17:14:30 +0400

    google-analytics

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,12 @@ import ReactDOM from 'react-dom/client'
 import {BrowserRouter, Route, Routes} from 'react-router-dom'
 import router from './routes'
 
+import ReactGA from 'react-ga4'
+
+ReactGA.initialize('G-VX9XMGW1W0')
+
+ReactGA.send({hitType: 'pageview', page: window.location.pathname})
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
