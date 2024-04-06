3ffcf50bb3f1388791e8ff4b14ca527757efde1f
Faisal Tariq <faisaltariq183@gmail.com>
2024-04-06 15:03:21 +0400

    refactor: seo

--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -1,22 +1,16 @@
 import {Toaster} from '@/components/ui/toaster'
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query'
 import {ReactNode} from 'react'
-import SeoProvider from './seo-provider'
-import {SocialIcon} from 'react-social-icons'
+
+import React from 'react'
+const WhatsAppIcon = React.lazy(() => import('@/components/whatsapp-icon'))
 
 const queryClient = new QueryClient()
 
 const AppProviders = ({children}: {children: ReactNode}) => {
   return (
     <QueryClientProvider client={queryClient}>
-      <a
-        href="https://wa.me/971555797971"
-        target="_blank"
-        className="fixed z-10 hover:scale-110 right-4 bottom-4"
-      >
-        <SocialIcon network="whatsapp" style={{height: 40, width: 40}} />
-      </a>
-      <SeoProvider />
+      <WhatsAppIcon />
       {children}
       <Toaster />
     </QueryClientProvider>
